081e455f6f968de49a68e258543c38bb254b3221
unknown <flyinglexmi@gmail.com>
2025-05-26 12:41:04 +0300

    izveidoju create maršrutu

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/create', [BookController::class, 'update']);
+Route::post('/books/create', [BookController::class, 'create']);
 Route::post('/books', [BookController::class, 'store']);
 Route::get('/books/{id}', [BookController::class, 'show']);
 Route::get('/books/{id}/edit', [BookController::class, 'edit']);
 Route::delete('/books/{id}/destroy', [BookController::class, 'destroy']);
+Route::get('/books/create', [BookController::class, 'update']);
